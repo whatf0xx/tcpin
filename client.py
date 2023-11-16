d8faf9572d5b7214488c16d7e623276f3ecd3eb4
whatf0xx <hfox022@gmail.com>
2023-11-16 21:28:19 +0100

    A simple tcp server which prints received text line by line

---
+++ client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from socket import socket, AF_INET, SOCK_STREAM
+
+HOST = "0.0.0.0"
+PORT = 1024
+
+PAYLOAD = """Here is some text to send to the server!\nNewlines are pretty fly!
+we can also add them like this, what happens then?\n\n"""
+
+with socket(AF_INET, SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    s.sendall(PAYLOAD.encode('utf-8'))
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::{
+    fmt,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:1024").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        if let Err(handling_error) = handle_connection(stream) {
+            println!("Ran into an unexpected error, {}.", handling_error);
+        }
+    }
+}
+
+fn handle_connection(stream: TcpStream) -> Result<(), HandlingError> {
+    let buf_reader = BufReader::new(stream);
+    let mut found_errors: bool = false;
+    let mut counter: i32 = 0;
+    buf_reader.lines().for_each(
+        |line| match line {
+            Ok(txt) => {println!("{}: {}", counter, txt);
+                        counter += 1},
+            Err(err) => {println!("Ran into a problem, looks like {:?}", err);
+                         found_errors = true},
+        }
+    );
+    if found_errors {Err(HandlingError::BufferError)} else {Ok(())}
+}
+
+enum HandlingError {
+    BufferError,
+
+}
+
+impl std::fmt::Display for HandlingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::BufferError => write!(f, "BufferError"),
+        }
+    }
 }
